25f141b68a6542e955b2f1aefc031c8729ed0e78
esi1 <barname22@gmail.com>
2019-09-28 11:55:47 +0200

    added ng-bootstrap

--- shopp/src/app/app.module.ts
+++ shopp/src/app/app.module.ts
@@ -3,20 +3,55 @@ import { NgModule } from "@angular/core";
 import { AngularFireModule } from "@angular/fire";
 import { AngularFireDatabaseModule } from "@angular/fire/database";
 import { AngularFireAuthModule } from "@angular/fire/auth";
+import { RouterModule } from "@angular/router";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { environment } from "src/environments/environment";
-import { BsNavbarComponent } from './components/bs-navbar/bs-navbar.component';
+import { BsNavbarComponent } from "./components/bs-navbar/bs-navbar.component";
+import { HomeComponent } from "./components/home/home.component";
+import { ProductsComponent } from "./components/products/products.component";
+import { ShoppingCartComponent } from "./components/shopping-cart/shopping-cart.component";
+import { CheckOutComponent } from "./components/check-out/check-out.component";
+import { OrderSuccessComponent } from "./components/order-success/order-success.component";
+import { MyOrdersComponent } from "./components/my-orders/my-orders.component";
+import { LoginComponent } from "./components/login/login.component";
+import { AdminProductsComponent } from "./components/admin/admin-products/admin-products.component";
+import { AdminOrdersComponent } from "./components/admin/admin-orders/admin-orders.component";
 
 @NgModule({
-  declarations: [AppComponent, BsNavbarComponent],
+  declarations: [
+    AppComponent,
+    BsNavbarComponent,
+    HomeComponent,
+    ProductsComponent,
+    ShoppingCartComponent,
+    CheckOutComponent,
+    OrderSuccessComponent,
+    MyOrdersComponent,
+    LoginComponent,
+    AdminProductsComponent,
+    AdminOrdersComponent
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
-    AngularFireAuthModule
+    AngularFireAuthModule,
+    RouterModule.forRoot([
+      { path: "", component: HomeComponent },
+      { path: "products", component: ProductsComponent },
+      { path: "shopping-cart", component: ShoppingCartComponent },
+      { path: "check-out", component: CheckOutComponent },
+      { path: "order-success", component: OrderSuccessComponent },
+      { path: "my/orders", component: MyOrdersComponent },
+      { path: "login", component: LoginComponent },
+      { path: "admin/products", component: AdminProductsComponent },
+      { path: "admin/orders", component: AdminOrdersComponent }
+    ]),
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
